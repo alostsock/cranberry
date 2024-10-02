027bd9f2b1d4352ef980af8c4f3918cc717907ab
alostsock <hi@alostsock.dev>
2024-10-02 15:11:50 -0400

    content

--- src/app.tsx
+++ src/app.tsx
@@ -1,43 +1,139 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
+import chroma from "chroma-js";
+import { useState } from "preact/hooks";
+import "./app.scss";
+
+const DEFAULT_COLORS = {
+  neutral: "#302c2c",
+  palette: {
+    yellow: "#ffdc40",
+    blue: "#45a4f9",
+    red: "#e4593e",
+    green: "#79cb3a",
+    teal: "#18aa96",
+  },
+} as const;
+
+const SHADING = {
+  linear: (x: number) => x,
+  easeIn: (x: number) => 1 - Math.cos((x * Math.PI) / 2),
+  easeOut: (x: number) => Math.sin((x * Math.PI) / 2),
+  easeInOut: (x: number) => (1 - Math.cos(x * Math.PI)) / 2,
+} as const;
+
+interface Color {
+  name: string;
+  code: string;
+  targetIndex: number;
+  shadingFunction: (x: number) => number;
+  whiteMixRatio: number;
+  blackMixRatio: number;
+}
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [neutral, _setNeutral] = useState<Color>({
+    name: "gray",
+    code: DEFAULT_COLORS.neutral,
+    targetIndex: 7,
+    shadingFunction: SHADING.easeOut,
+    whiteMixRatio: 0.9,
+    blackMixRatio: 0.85,
+  });
+
+  const [palette, _setPalette] = useState<Color[]>(
+    Object.entries(DEFAULT_COLORS.palette).map(([name, code]) => ({
+      name,
+      code,
+      targetIndex: 5,
+      shadingFunction: SHADING.easeInOut,
+      whiteMixRatio: 0.9,
+      blackMixRatio: 0.85,
+    })),
+  );
+
+  const colors = [neutral, ...palette];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
+      <Swatches colors={colors} />
+      <CssVariablesOutput colors={colors} />
     </>
-  )
+  );
+}
+
+function Swatches({ colors }: { colors: Color[] }) {
+  return (
+    <section className="Swatches">
+      {colors.map((color) => (
+        <Shades key={color.name} colors={getShades(color)} />
+      ))}
+    </section>
+  );
+}
+
+function getShades(color: Color): chroma.Color[] {
+  const { code, targetIndex, shadingFunction, whiteMixRatio, blackMixRatio } =
+    color;
+
+  const lightestShade = chroma(code).mix("#fff", whiteMixRatio, "lab");
+  const scale = chroma.scale([lightestShade, code]).mode("lab");
+  const length = targetIndex + 1;
+  const indices = [...Array(length).keys()];
+  const colors = indices.map((i) => {
+    const value = shadingFunction(i / length);
+    return scale(value);
+  });
+
+  const darkestShade = chroma(code).mix("#000", blackMixRatio, "lab");
+  const extendedScale = chroma.scale([code, darkestShade]).mode("lab");
+  const extensionLength = 10 - length;
+  const extendedIndices = [...Array(extensionLength).keys()].map((i) => i + 1);
+  const extendedColors = extendedIndices.map((i) => {
+    const value = SHADING.linear(i / extensionLength);
+    return extendedScale(value);
+  });
+
+  return [...colors, ...extendedColors];
+}
+
+function formatHslStr(color: chroma.Color): string {
+  const [h, s, l] = color.hsl();
+  return `
+hsl(${h.toFixed(2)}
+    ${s.toFixed(2)}%
+    ${l.toFixed(2)}%)`.trim();
+}
+
+function Shades({ colors }: { colors: chroma.Color[] }) {
+  return (
+    <div className="Shades">
+      {colors.map((color, i) => {
+        return (
+          <button key={i} className="shade" style={{ background: color.css() }}>
+            <pre>{formatHslStr(color)}</pre>
+          </button>
+        );
+      })}
+    </div>
+  );
+}
+
+function CssVariablesOutput({ colors }: { colors: Color[] }) {
+  const inner = colors
+    .flatMap((color) => {
+      const shades = getShades(color);
+      return shades.map(
+        (shade, i) => `  --${color.name}-${i}: ${shade.css("hsl")};`,
+      );
+    })
+    .join("\n");
+
+  const outer = `:root {\n${inner}\n}`;
+
+  return (
+    <section className="CssVariablesOutput">
+      <pre>
+        <code>{outer}</code>
+      </pre>
+    </section>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
-import { render } from 'preact'
-import { App } from './app.tsx'
-import './index.css'
+import { render } from "preact";
+import { App } from "./app.tsx";
+import "./normalize.css";
+import "./variables.css";
+import "./index.css";
 
-render(<App />, document.getElementById('app')!)
+render(<App />, document.getElementById("app")!);
