1b7daf724bc7f40c290ad33aeb0f84e36e321d45
alostsock <hi@alostsock.dev>
2024-10-02 23:26:35 -0400

    add color pickers

--- src/app.tsx
+++ src/app.tsx
@@ -2,17 +2,6 @@ import chroma from "chroma-js";
 import { useState } from "preact/hooks";
 import "./app.scss";
 
-const DEFAULT_COLORS = {
-  neutral: "#302c2c",
-  palette: {
-    yellow: "#ffdc40",
-    blue: "#45a4f9",
-    red: "#e4593e",
-    green: "#79cb3a",
-    teal: "#18aa96",
-  },
-} as const;
-
 const SHADING = {
   linear: (x: number) => x,
   easeIn: (x: number) => 1 - Math.cos((x * Math.PI) / 2),
@@ -20,42 +9,121 @@ const SHADING = {
   easeInOut: (x: number) => (1 - Math.cos(x * Math.PI)) / 2,
 } as const;
 
+type ShadingFunction = keyof typeof SHADING;
+
 interface Color {
   name: string;
-  code: string;
+  hex: string;
   targetIndex: number;
-  shadingFunction: (x: number) => number;
+  shadingFunction: ShadingFunction;
   whiteMixRatio: number;
   blackMixRatio: number;
 }
 
-export function App() {
-  const [neutral, _setNeutral] = useState<Color>({
+const DEFAULT_COLORS = [
+  {
     name: "gray",
-    code: DEFAULT_COLORS.neutral,
+    hex: "#302c2c",
     targetIndex: 7,
-    shadingFunction: SHADING.easeOut,
+    shadingFunction: "easeOut",
     whiteMixRatio: 0.9,
     blackMixRatio: 0.85,
-  });
+  },
+  ...Object.entries({
+    yellow: "#ffdc40",
+    red: "#e4593e",
+    teal: "#18aa96",
+    blue: "#45a4f9",
+    green: "#79cb3a",
+  }).map(([name, hex]) => ({
+    name,
+    hex,
+    targetIndex: 5,
+    shadingFunction: "easeInOut",
+    whiteMixRatio: 0.9,
+    blackMixRatio: 0.85,
+  })),
+] as const;
 
-  const [palette, _setPalette] = useState<Color[]>(
-    Object.entries(DEFAULT_COLORS.palette).map(([name, code]) => ({
-      name,
-      code,
-      targetIndex: 5,
-      shadingFunction: SHADING.easeInOut,
-      whiteMixRatio: 0.9,
-      blackMixRatio: 0.85,
-    })),
-  );
-
-  const colors = [neutral, ...palette];
+export function App() {
+  const [palette, setPalette] = useState<Color[]>(DEFAULT_COLORS);
 
   return (
     <>
-      <Swatches colors={colors} />
-      <CssVariablesOutput colors={colors} />
+      <ColorPickers palette={palette} setPalette={setPalette} />
+      <Swatches colors={palette} />
+      <CssVariablesOutput colors={palette} />
+    </>
+  );
+}
+
+function ColorPickers({
+  palette,
+  setPalette,
+}: {
+  palette: Color[];
+  setPalette: (colors: Color[]) => void;
+}) {
+  return (
+    <section className="ColorPickers">
+      {palette.map((color, i) => (
+        <ColorPicker
+          key={`${i}-${color.name}`}
+          id={`${i}-${color.name}`}
+          name={color.name}
+          hex={color.hex}
+          setName={(name) => {
+            const newPalette = JSON.parse(JSON.stringify(palette));
+            newPalette[i] = {
+              ...newPalette[i],
+              name,
+            };
+            setPalette(newPalette);
+          }}
+          setHex={(hex) => {
+            const newPalette = JSON.parse(JSON.stringify(palette));
+            newPalette[i] = {
+              ...newPalette[i],
+              hex,
+            };
+            setPalette(newPalette);
+          }}
+        />
+      ))}
+    </section>
+  );
+}
+
+interface ColorPickerProps {
+  id: string;
+  name: string;
+  hex: string;
+  setName: (label: string) => void;
+  setHex: (hex: string) => void;
+}
+
+function ColorPicker({ id, name, hex, setName, setHex }: ColorPickerProps) {
+  const nameInputId = `${id}-name`;
+  const hexInputId = `${id}-hex`;
+
+  return (
+    <>
+      <input
+        id={nameInputId}
+        value={name}
+        onChange={(e) => setName(e.currentTarget.value ?? "")}
+      />
+      <input
+        id={hexInputId}
+        type="color"
+        value={hex}
+        onChange={(e) => setHex(e.currentTarget.value ?? "#000")}
+      />
+      <label for={hexInputId}>
+        <pre>
+          <code>{hex}</code>
+        </pre>
+      </label>
     </>
   );
 }
@@ -71,20 +139,20 @@ function Swatches({ colors }: { colors: Color[] }) {
 }
 
 function getShades(color: Color): chroma.Color[] {
-  const { code, targetIndex, shadingFunction, whiteMixRatio, blackMixRatio } =
+  const { hex, targetIndex, shadingFunction, whiteMixRatio, blackMixRatio } =
     color;
 
-  const lightestShade = chroma(code).mix("#fff", whiteMixRatio, "lab");
-  const scale = chroma.scale([lightestShade, code]).mode("lab");
+  const lightestShade = chroma(hex).mix("#fff", whiteMixRatio, "lab");
+  const scale = chroma.scale([lightestShade, hex]).mode("lab");
   const length = targetIndex + 1;
   const indices = [...Array(length).keys()];
   const colors = indices.map((i) => {
-    const value = shadingFunction(i / length);
+    const value = SHADING[shadingFunction](i / length);
     return scale(value);
   });
 
-  const darkestShade = chroma(code).mix("#000", blackMixRatio, "lab");
-  const extendedScale = chroma.scale([code, darkestShade]).mode("lab");
+  const darkestShade = chroma(hex).mix("#000", blackMixRatio, "lab");
+  const extendedScale = chroma.scale([hex, darkestShade]).mode("lab");
   const extensionLength = 10 - length;
   const extendedIndices = [...Array(extensionLength).keys()].map((i) => i + 1);
   const extendedColors = extendedIndices.map((i) => {
